ea15220cda6fff509987b3bfb1fb9998be02a929
holance <holance@gmail.com>
2016-07-27 00:52:04 -0700

    up

--- DrawingCore/DrawingCoreTester/MainPage.xaml.cs
+++ DrawingCore/DrawingCoreTester/MainPage.xaml.cs
@@ -35,21 +35,21 @@ namespace DrawingCoreTester
             Loaded += MainPage_Loaded;
             Unloaded += MainPage_Unloaded;
             mTimer.Tick += Timer_Tick;
-            mTimer.Interval = TimeSpan.FromMilliseconds(20);
+            mTimer.Interval = TimeSpan.FromMilliseconds(10);
         }
 
-        private byte[] buffer = new byte[1];
+        private byte[] buffer = new byte[4];
         private void Timer_Tick(object sender, object e)
         {
             var rect = new Rect(mRnd.Next(0, (int)canvas.ActualWidth - 400), mRnd.Next(0, (int)canvas.ActualHeight - 400), 400, 400);
-            for (int i=0; i<100; ++i)
+            for (int i=0; i<10; ++i)
             {
                 var cmd = new SimpleDrawing();
                 cmd.Width = mRnd.Next(5, (int)rect.Width);
-                cmd.Height = mRnd.Next(5, (int)rect.Height);
-                cmd.Center = new System.Numerics.Vector2(mRnd.Next((int)rect.Left, (int)(rect.Left+rect.Width)), mRnd.Next((int)rect.Left, (int)(rect.Left + rect.Width)));
+                cmd.Height = cmd.Width;
+                cmd.Center = new System.Numerics.Vector2((float)(rect.Width/2+rect.Left), (float)(rect.Height/2+rect.Top));
                 mRnd.NextBytes(buffer);
-                cmd.Color = ColorHelper.FromArgb(buffer[0], buffer[0], buffer[0], buffer[0]);
+                cmd.Color = Color.FromArgb(buffer[0], buffer[1], buffer[2], buffer[3]);
                 mDrawingEngine.PushJob(cmd);
             }
             mDrawingEngine.Invalidate(rect);
@@ -69,7 +69,8 @@ namespace DrawingCoreTester
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            mDrawingEngine.Clear(Colors.Black);
+            mRnd.NextBytes(buffer);
+            mDrawingEngine.Clear(Color.FromArgb(buffer[0], buffer[1], buffer[2], buffer[3]));
         }
     }
 }
